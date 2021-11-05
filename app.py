46d5b8aef954f617386e996a82e273fd6007c057
tenaciouscri <cridelve@gmail.com>
2021-11-05 14:19:15 +0100

    Added a few more function exercises

--- app.py
+++ app.py
@@ -181,4 +181,126 @@ def myfunc(my_string):
 print(myfunc("This is the beginning of the rest of your life."))
 
 # ----------------------------------------------------
+# FURTHER FUNCTIONS PRACTICE
 
+# Lesser of two evens: write a function that
+# returns the lesser of two given numbers if both
+# numbers are even, but returns the greater if one
+# or both numbers are odd.
+
+def lesser_of_two_evens(num1, num2):
+    if (num1 % 2 == 0) and (num2 % 2 == 0):
+        # Both numbers are even
+        if num1 < num2: # Simplify this part with min() function
+            result = num1
+        else:
+            result = num2
+    else:
+        # One or both numbers are odd
+        if num1 > num2: # Simplify this part with max() function
+            result = num1
+        else:
+            result = num2
+    return result
+
+# Simplified version
+
+def lesser_of_two_evens(num1, num2):
+    if (num1 % 2 == 0) and (num2 % 2 == 0):
+        result = min(num1, num2)
+    else:
+        result = max(num1, num2)
+    return result
+
+# Simplified even further (MOST EFFICIENT)
+
+def lesser_of_two_evens(num1, num2):
+    if (num1 % 2 == 0) and (num2 % 2 == 0):
+        return min(num1, num2)
+    else:
+        return max(num1, num2)
+
+print(lesser_of_two_evens(2, 4)) # Two evens
+print(lesser_of_two_evens(2, 5)) # Even and odd
+print(lesser_of_two_evens(5, 7)) # Two odds
+
+# Animal crackers: write a function that takes a
+# two-word string and returns True if both words
+# begin with the same letter.
+
+def animal_crackers(string):
+    # could also be string.upper().split()
+    word_list = string.lower().split()
+    
+    first = word_list[0]
+    second = word_list[1]
+    
+    return first[0] == second[0]
+
+# Simplified version
+
+def animal_crackers(string):
+    word_list = string.lower().split()
+    
+    return word_list[0][0] == word_list[1][0]
+
+print(animal_crackers("Levelheaded Llama"))
+print(animal_crackers("Crazy Kangaroo"))
+print(animal_crackers("Ambitious armadillo"))
+print(animal_crackers("strong Penguin"))
+
+# Makes twenty: given two integers, returns True
+# if the sum of the integers is 20 or if one of
+# the integers is 20. If not, return False.
+
+def makes_twenty(num1, num2):
+    if sum((num1, num2)) == 20:
+        return True
+    elif num1 == 20 or num2 == 20:
+        return True
+    else:
+        return False
+
+# Simplified version
+
+def makes_twenty(num1, num2):
+    return sum((num1, num2)) == 20 or num1 == 20 or num2 == 20
+
+print(makes_twenty(20, 10))
+print(makes_twenty(2, 3))
+print(makes_twenty(8, 12))
+
+# Old Macdonald: write a function that capitalizes
+# the first and fourth letters of a name.
+
+def old_macdonald(name):
+    
+    first_letter = name[0]
+    in_between = name[1:3]
+    fourth_letter = name[3]
+    rest = name[4:]
+    
+    return first_letter.upper() + in_between + fourth_letter.upper() + rest
+
+# Other method (separating in parts + capitalize())
+
+def old_macdonald(name):
+    
+    first_part = name[:3]
+    second_part = name[3:]
+    
+    return first_part.capitalize() + second_part.capitalize()
+
+print(old_macdonald("macdonald"))
+
+# Master Yoda: given a sentence, return a sentence
+# with the words reversed.
+
+def master_yoda(text):
+    word_list = text.split()
+    reversed_word_list = word_list[::-1]
+    yoda_talk = " ".join(reversed_word_list)
+    return yoda_talk
+
+print(master_yoda("I am home"))
+print(master_yoda("We are ready"))
